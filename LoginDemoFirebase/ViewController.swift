598a71d963edd3bdca1c99c4e452656e060aad9a
vic8tor <timongrinni@gmail.com>
2022-01-30 15:19:39 +0300

    Initial Commit

---
+++ LoginDemoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginDemoFirebase
+//
+//  Created by Victor on 30.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
